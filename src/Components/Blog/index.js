b909f504f55d3b094093daf6547137ce8c9c3ff8
Yanek-K <yanek.dev@gmail.com>
2025-05-21 15:28:48 -0400

    styling for blog

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -3,24 +3,35 @@ import "./index.css";
 
 const Bloglist = ({ posts }) => {
   return (
-    <div className="blog-list">
-      {posts.map(({ slug, title, excerpt, heroImage, date, author, tags }) => (
-        <article key={slug} className="post-card">
-          <a href={`/blog/${slug}/`}>
-            {heroImage && (
-              <img src={heroImage} alt={title} className="card-img" />
+    <div className="blog-section">
+      <div className="blog__wrap">
+        <aside className="blog__nav">
+          <p>link</p>
+          <p>link</p>
+          <p>link</p>
+          <p>link</p>
+        </aside>
+        <div className="blog__content">
+          <div className="blog-list">
+            {posts.map(
+              ({ slug, title, excerpt, heroImage, date, author, tags }) => (
+                <article key={slug} className="post-card">
+                  <a href={`/blog/${slug}/`}>
+                    <div className="card-data">
+                      <h3>{title}</h3>
+                      <p className="excerpt">{excerpt}</p>
+                      <div className="meta">
+                        <span>{new Date(date).toLocaleDateString()}</span> —{" "}
+                        <span>{author}</span>
+                      </div>
+                    </div>
+                  </a>
+                </article>
+              )
             )}
-            <div className="card-data">
-              <h3>{title}</h3>
-              <p className="excerpt">{excerpt}</p>
-              <div className="meta">
-                <span>{new Date(date).toLocaleDateString()}</span> —{" "}
-                <span>{author}</span>
-              </div>
-            </div>
-          </a>
-        </article>
-      ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
